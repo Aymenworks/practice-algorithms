d0062e5c3aca61207c92716d9649df3c9af01544
Aymen Rebouh <aymenmse@gmail.com>
2019-01-08 16:57:06 +0900

    Add bubble and insertion sort

--- PracticeAlgorithms/PracticeAlgorithms/PracticeAlgorithmsTests/PracticeAlgorithmsTests.swift
+++ PracticeAlgorithms/PracticeAlgorithms/PracticeAlgorithmsTests/PracticeAlgorithmsTests.swift
@@ -12,56 +12,98 @@ import XCTest
 class PracticeAlgorithmsTests: XCTestCase {
 
     var orderedIntegers: [Int] = []
-    
+    var randomIntegers: [Int] = []
+
     override func setUp() {
-        for i in 0..<10_000_000 {
+        for i in 0..<20_000 {
             orderedIntegers.append(i)
+            randomIntegers.append(Int(arc4random()))
         }
     }
 
     override func tearDown() {
         orderedIntegers = []
+        randomIntegers = []
     }
 
 
-    // Linear time, that means that the search speed will depend on the input size
-    // Meaning that the search algorithm become less efficient as its input size grow
-    func testBruteForceSearch() {
-        let numberToFind = 5_000_000
+//    // Linear time, that means that the search speed will depend on the input size
+//    // Meaning that the search algorithm become less efficient as its input size grow
+//    func testBruteForceSearch() {
+//        let numberToFind = 5_000_000
+//        self.measure {
+//            for number in orderedIntegers {
+//                if number == numberToFind {
+//                    print("brute force found")
+//                    break
+//                }
+//            }
+//        }
+//    }
+//
+//    // We know that if we are looking for 10, it doesn't make sense to search from 11 - 1000000 or 0-9
+//    // So the idea is to avoid ununcessary search by looking at the middle all the time
+//    func testBinarySearch() {
+//        let numberToFind = 5_000_000
+//        self.measure {
+//            var finish = false
+//            var minIndex = 0
+//            var maxIndex = orderedIntegers.count-1
+//            while !finish {
+//                let midIndex = (minIndex+maxIndex)/2
+//                let middleValue = orderedIntegers[midIndex]
+//
+//                if middleValue == numberToFind {
+//                    finish = true
+//                } else if middleValue < numberToFind {
+//                    minIndex = midIndex + 1
+//                } else {
+//                    maxIndex = midIndex - 1
+//                }
+//
+//                if minIndex == maxIndex {
+//                    finish = true
+//                }
+//            }
+//        }
+//    }
+//
+    // 0.481
+    func testNativeSort() {
         self.measure {
-            for number in orderedIntegers {
-                if number == numberToFind {
-                    print("brute force found")
-                    break
+            print("testNativeSort")
+            randomIntegers.sort()
+        }
+    }
+    
+    // 3.280
+    func testBubbleSort() {
+        self.measure {
+            var isSorted = true
+            for i in 1..<randomIntegers.count {
+                isSorted = true
+                for j in stride(from: randomIntegers.count-1, to: i+1, by: -1) {
+                    if randomIntegers[j] < randomIntegers[j-1] {
+                        randomIntegers.swapAt(j, j-1)
+                        isSorted = false
+                    }
+                }
+                
+                if isSorted {
+                    return
                 }
             }
         }
     }
     
-    // We know that if we are looking for 10, it doesn't make sense to search from 11 - 1000000 or 0-9
-    // So the idea is to avoid ununcessary search by looking at the middle all the time
-    func testBinarySearch() {
-        let numberToFind = 5_000_000
+    // 1.369
+    func testInsertionSort() {
         self.measure {
-            var finish = false
-            var minIndex = 0
-            var maxIndex = orderedIntegers.count-1
-            while !finish {
-                let midIndex = (minIndex+maxIndex)/2
-                let middleValue = orderedIntegers[midIndex]
-                
-                if middleValue == numberToFind {
-                    finish = true
-                    print("binary search found")
-                } else if middleValue < numberToFind {
-                    minIndex = midIndex + 1
-                } else {
-                    maxIndex = midIndex - 1
-                }
-                
-                if minIndex == maxIndex {
-                    finish = true
-                    print("not found")
+            for i in 1..<randomIntegers.count {
+                var j = i
+                while j > 0 && randomIntegers[j] < randomIntegers[j-1] {
+                    randomIntegers.swapAt(j, j-1)
+                    j -= 1
                 }
             }
         }
