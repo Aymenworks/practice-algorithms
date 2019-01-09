45676ee3151c588d083607ecb1966282156b726a
Aymen Rebouh <aymenmse@gmail.com>
2019-01-09 17:14:53 +0900

    Add selection sort

--- PracticeAlgorithms/PracticeAlgorithms/PracticeAlgorithmsTests/PracticeAlgorithmsTests.swift
+++ PracticeAlgorithms/PracticeAlgorithms/PracticeAlgorithmsTests/PracticeAlgorithmsTests.swift
@@ -15,7 +15,7 @@ class PracticeAlgorithmsTests: XCTestCase {
     var randomIntegers: [Int] = []
 
     override func setUp() {
-        for i in 0..<20_000 {
+        for i in 0..<10_000 {
             orderedIntegers.append(i)
             randomIntegers.append(Int(arc4random()))
         }
@@ -26,48 +26,6 @@ class PracticeAlgorithmsTests: XCTestCase {
         randomIntegers = []
     }
 
-
-//    // Linear time, that means that the search speed will depend on the input size
-//    // Meaning that the search algorithm become less efficient as its input size grow
-//    func testBruteForceSearch() {
-//        let numberToFind = 5_000_000
-//        self.measure {
-//            for number in orderedIntegers {
-//                if number == numberToFind {
-//                    print("brute force found")
-//                    break
-//                }
-//            }
-//        }
-//    }
-//
-//    // We know that if we are looking for 10, it doesn't make sense to search from 11 - 1000000 or 0-9
-//    // So the idea is to avoid ununcessary search by looking at the middle all the time
-//    func testBinarySearch() {
-//        let numberToFind = 5_000_000
-//        self.measure {
-//            var finish = false
-//            var minIndex = 0
-//            var maxIndex = orderedIntegers.count-1
-//            while !finish {
-//                let midIndex = (minIndex+maxIndex)/2
-//                let middleValue = orderedIntegers[midIndex]
-//
-//                if middleValue == numberToFind {
-//                    finish = true
-//                } else if middleValue < numberToFind {
-//                    minIndex = midIndex + 1
-//                } else {
-//                    maxIndex = midIndex - 1
-//                }
-//
-//                if minIndex == maxIndex {
-//                    finish = true
-//                }
-//            }
-//        }
-//    }
-//
     // 0.481
     func testNativeSort() {
         self.measure {
@@ -76,8 +34,7 @@ class PracticeAlgorithmsTests: XCTestCase {
         }
     }
     
-    // 3.280
-    func testBubbleSort() {
+    func testBubbleSortOnRandomNumbers() {
         self.measure {
             var isSorted = true
             for i in 1..<randomIntegers.count {
@@ -96,14 +53,78 @@ class PracticeAlgorithmsTests: XCTestCase {
         }
     }
     
-    // 1.369
-    func testInsertionSort() {
+    func testBubbleSortOnSortedNumbers() {
+        self.measure {
+            var isSorted = true
+            for i in 1..<orderedIntegers.count {
+                isSorted = true
+                for j in stride(from: orderedIntegers.count-1, to: i+1, by: -1) {
+                    if orderedIntegers[j] < orderedIntegers[j-1] {
+                        orderedIntegers.swapAt(j, j-1)
+                        isSorted = false
+                    }
+                }
+                
+                if isSorted {
+                    return
+                }
+            }
+        }
+    }
+    
+    func testInsertionSortOnRandomNumbers() {
         self.measure {
             for i in 1..<randomIntegers.count {
                 var j = i
                 while j > 0 && randomIntegers[j] < randomIntegers[j-1] {
                     randomIntegers.swapAt(j, j-1)
                     j -= 1
+                }
+            }
+        }
+    }
+    
+    func testInsertionSortOnSortedNumbers() {
+        self.measure {
+            for i in 1..<orderedIntegers.count {
+                var j = i
+                while j > 0 && orderedIntegers[j] < orderedIntegers[j-1] {
+                    orderedIntegers.swapAt(j, j-1)
+                    j -= 1
+                }
+            }
+        }
+    }
+    
+    func testSelectionSortOnRandomNumbers() {
+        self.measure {
+            for i in 0..<randomIntegers.count {
+                var indexSmallestItem = i
+                for j in i+1..<randomIntegers.count {
+                    if randomIntegers[j] < randomIntegers[indexSmallestItem] {
+                        indexSmallestItem = j
+                    }
+                }
+                
+                if i != indexSmallestItem {
+                    randomIntegers.swapAt(indexSmallestItem, i)
+                }
+            }
+        }
+    }
+    
+    func testSelectionSortOnOrderedNumbers() {
+        self.measure {
+            for i in 0..<orderedIntegers.count {
+                var indexSmallestItem = i
+                for j in i+1..<orderedIntegers.count {
+                    if orderedIntegers[j] < orderedIntegers[indexSmallestItem] {
+                        indexSmallestItem = j
+                    }
+                }
+                
+                if i != indexSmallestItem {
+                    orderedIntegers.swapAt(indexSmallestItem, i)
                 }
             }
         }
---
+++ PracticeAlgorithms/PracticeAlgorithms/PracticeAlgorithmsTests/SearchAlgorithmsTests.swift
@@ -0,0 +1,69 @@
+//
+//  SearchAlgorithmsTests.swift
+//  PracticeAlgorithmsTests
+//
+//  Created by Rebouh Aymen on 09/01/2019.
+//  Copyright © 2019 Aymen Rebouh. All rights reserved.
+//
+
+import XCTest
+
+class SearchAlgorithmsTests: XCTestCase {
+
+    let maxNumber = 10_000
+    var orderedIntegers: [Int] = []
+    var randomIntegers: [Int] = []
+    
+    override func setUp() {
+        for i in 0..<maxNumber {
+            orderedIntegers.append(i)
+            randomIntegers.append(Int(arc4random()))
+        }
+    }
+    
+    override func tearDown() {
+        orderedIntegers = []
+        randomIntegers = []
+    }
+    
+    // Linear time, that means that the search speed will depend on the input size
+    // Meaning that the search algorithm become less efficient as its input size grow
+    func testBruteForceSearch() {
+        let numberToFind = maxNumber/2
+        self.measure {
+            for number in orderedIntegers {
+                if number == numberToFind {
+                    print("brute force found")
+                    break
+                }
+            }
+        }
+    }
+    
+    // We know that if we are looking for 10, it doesn't make sense to search from 11 - 1000000 or 0-9
+    // So the idea is to avoid ununcessary search by looking at the middle all the time
+    func testBinarySearch() {
+        let numberToFind = maxNumber/2
+        self.measure {
+            var finish = false
+            var minIndex = 0
+            var maxIndex = orderedIntegers.count-1
+            while !finish {
+                let midIndex = (minIndex+maxIndex)/2
+                let middleValue = orderedIntegers[midIndex]
+                
+                if middleValue == numberToFind {
+                    finish = true
+                } else if middleValue < numberToFind {
+                    minIndex = midIndex + 1
+                } else {
+                    maxIndex = midIndex - 1
+                }
+                
+                if minIndex == maxIndex {
+                    finish = true
+                }
+            }
+        }
+    }
+}
